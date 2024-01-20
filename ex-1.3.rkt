f11b11e2e959e3a36e518fa0b16b21c4f581c7b6
Andre Myers <andre@Andres-MBP.local>
2024-01-20 00:54:35 -0800

    damn i think i did it

--- ex-1.3.rkt
+++ ex-1.3.rkt
@@ -2,7 +2,8 @@
 
 #| if z is bigger than the bigger of x and y
 then they are the two biggest. if z is smaller then
-z must be compared against the smaller of x and y |#
+z must be compared against the smaller of x and y
+[(< z (if (< x y) x y)) (+ (square x) (square y))]|#
 
 (define (square x) (* x x))
 (define (the-greater-of x y) (if (>= x y) x y))
@@ -10,6 +11,7 @@ z must be compared against the smaller of x and y |#
 (define (do-thing x y z)
   (cond
     [(>= z (the-greater-of x y)) (+ (square z) (square (the-greater-of x y)))]
-    [(< z (if (< x y) x y)) (+ (square x) (square y))]))
+    [(>= y (the-greater-of x z)) (+ (square y) (square (the-greater-of x z)))]
+    [(>= x (the-greater-of y z)) (+ (square x) (square (the-greater-of y z)))]))
 
-(do-thing 12 12 12)
+(do-thing 1 0 0)
