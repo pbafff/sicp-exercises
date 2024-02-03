a3a962a76509f02ccf189e8e3dd19be93a5bd257
Andre Myers <andre.myers99@gmail.com>
2024-02-03 11:38:52 -0800

    starting over

--- ex-1.7.rkt
+++ ex-1.7.rkt
@@ -1,54 +1,43 @@
 #lang sicp
 
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
 (define (square x) (* x x))
 
 (define (good-enough? guess x)
   (< (abs (- (square guess) x )) 0.001))
 
-;;needs to fail to determine that the difference between x and guess^2 is less than 0.001
+(define (average x y)
+  (/ (+ x y) 2))
 
-(- 1 (+ 1000000000000000 1))
+(define (improve guess x)      ;;provides change
+  (average guess (/ x guess)))
 
-(- 1 (+ 1000000000000000 2))
+(define (newline) (display "\n"))
 
-(- 1 (+ 1000000000000000 3))
+(define (sqrt-iter guess x)
+  (display "GUESS: ")
+  (display guess) ;;provides iteration
+  (display " off by: ")
+  (display (- (square guess) x))
+  (newline)
+  (if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
 
-(< (abs (- 1.0 (+ 1 0.00001))) 0.001)
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
 
-(- 1.0 (+ 1000000000000000 2))
+;;(sqrt 1111111111111111111)
 
-(- 1.0 (+ 1000000000000000 3))
-
-;;answer does not lie in this difference
-;;perhaps i need to let go of keeping guess at 1.0
-;;can * fail to square a number such that the produced value is more than 0.001 away from being correct
-;;if x is big or small enough then maybe, when squared, the resu
-;;take a number, sqrt it, square it back, take difference from original number, is it off by more than 0.001
-;;start with √x
-
-(< (abs (- (* 0.000005 0.000005) 0.000005)) 0.001)
-
-#;(define (test x)
-  (< (abs (- (* x x) x)) 0.001))
-
-;;try solving next part and use that to answer this part?
-
-(< (abs (- 1000000000000000000000000000000000000000000000000000000 (+ 0.1  1000000000000000000000000000000000000000000000000000000))) 0.001)
-
-(define (test x diff)
-  (display (abs (- x (+ x diff))))
-  (< (abs (- x (+ x diff))) 0.001))
-
-(test 0.00000000000000000000000000000000000000000000000000000000000000000000000000000001 0.0001)
-
-(test 10000 0.0001)
-
-(test 1000 0.0001)
-
-(test 100 0.0001)
-
-(test 10 0.0001)
-
-(test 1 0.0001)
-
-(test 0 0.0001)
+(- 1111111111111111111 1.0) ;;gives 1111111111111111200 instead of 1111111111111111110
+(improve 1.0 1111111111111111111)
+(good-enough? 1054092553.3894598 1111111111111111111)
+(abs (- (square 1054092553.3894598) 1111111111111111111))
+(square 1054092553.3894598)
+(/ 1111111111111111111 1054092553.3894598)
+(+ 1054092553.3894597 1054092553.3894598)
+(/ 2108185106.7789197 2)
