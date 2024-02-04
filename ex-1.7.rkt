beb7b137269622d5c8f1e0b1c72325053da91611
Andre Myers <andre@Andres-MBP.local>
2024-02-03 19:23:06 -0800

    moving on to small numbers

--- ex-1.7.rkt
+++ ex-1.7.rkt
@@ -12,15 +12,15 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (improve guess x)      ;;provides change
+(define (improve guess x)      
   (average guess (/ x guess)))
 
 (define (newline) (display "\n"))
 
 (define (sqrt-iter guess x)
   (display "GUESS: ")
-  (display guess) ;;provides iteration
-  (display " off by: ")
+  (display guess)
+  (display " GUESS^2 off by: ")
   (display (- (square guess) x))
   (newline)
   (if (good-enough? guess x)
@@ -31,13 +31,22 @@
 (define (sqrt x)
   (sqrt-iter 1.0 x))
 
-;;(sqrt 1111111111111111111)
+#;
+(sqrt 1111111111111111111)
+;;this is an infinite loop. guess eventually reaches a value that should trigger good-enough? to return #t (1054092553.3894745), but #f is returned so guess is refined one more time (1054092553.3894598), after which it ceases to change and sqrt-iter calls itself infinitely.
 
-(- 1111111111111111111 1.0) ;;gives 1111111111111111200 instead of 1111111111111111110
-(improve 1.0 1111111111111111111)
-(good-enough? 1054092553.3894598 1111111111111111111)
-(abs (- (square 1054092553.3894598) 1111111111111111111))
+(square 1054092553.3894745)
+;;returns 1111111111111142100.0, much too large to get a #t out of good-enough?
+
 (square 1054092553.3894598)
-(/ 1111111111111111111 1054092553.3894598)
-(+ 1054092553.3894597 1054092553.3894598)
-(/ 2108185106.7789197 2)
+;;returns 1111111111111111300.0, also too large
+
+;;both of these calculations are incorrect but it doesn't matter; even if they weren't, squaring them would still produce values too big
+
+(improve 1054092553.3894598 1111111111111111111)
+;;returns the same guess. being limited to just 7 decimal places prevents improve from producing a guess that can be squared to get a number close enough to 1111111111111111111. so, we get suck in an infinite loop.
+
+(sqrt 0.0000000000000000001)
+;;returns 0.03125 which is obviously wrong
+
+(square 0.03125)
